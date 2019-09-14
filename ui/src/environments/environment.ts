ca2245a6e35f091dafa23d7e685f9c6b4cd357f7
Ivan Butrim <vbutrim@gmail.com>
2019-09-14 15:00:30 +0300

    simple rest ui service

--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  hostName: 'localhost',
+  hostPort: '8090',
+  apiPrefix: 'api/v1'
 };
 
 /*
