42ceedd580aba78e619b6c0ca294ebec92ba39d9
zilberoman <roman@silvercom.ru>
2019-09-14 17:29:32 +0300

    removed useless elems

--- ui/src/app/layout/components/header/header.component.spec.ts
+++ ui/src/app/layout/components/header/header.component.spec.ts
@@ -1,6 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { TranslateModule } from '@ngx-translate/core';
 
 import { HeaderComponent } from './header.component';
 import { LayoutModule } from '../../layout.module';
@@ -13,9 +12,8 @@ describe('HeaderComponent', () => {
     TestBed.configureTestingModule({
       imports: [
         LayoutModule,
-        RouterTestingModule,
-        TranslateModule.forRoot(),
-      ],
+        RouterTestingModule
+      ]
     })
     .compileComponents();
   }));
